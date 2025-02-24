016b00b22c77a00fc42915c7e6e19c00a95c987b
AvinashRaj28 <avinashsinghrajput8392@gmail.com>
2025-02-24 14:50:35 +0530

    trying the fix the background image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: "export",
+    basePath: "/weather-app",
+    assetPrefix: "/weather-app/",
+    images: {
+      unoptimized: true, // Ensures images work on GitHub Pages
+    },
+  };
+  
+  module.exports = nextConfig;
+  
